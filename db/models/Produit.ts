815aa1f0609296b6c2055f310ae5cc6a83ecb80b
Mickael <mickael@popina.com>
2022-03-07 15:33:29 +0100

    Creation des modèles de données

---
+++ db/models/Produit.ts
@@ -0,0 +1,30 @@
+import { DataTypes } from "@sequelize/core";
+import sequelize from "../database";
+
+const Produit = sequelize.define('Produit', {
+    id_produit: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+    },
+    nom: {
+        type: DataTypes.STRING,
+    },
+    prix: {
+        type: DataTypes.DECIMAL,
+    },
+    description: {
+        type: DataTypes.TEXT,
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+    },
+    ref_categorie: {
+        type: DataTypes.INTEGER,
+    },
+},
+    {
+        timestamps: false,
+    }
+);
+
+export default Produit;
