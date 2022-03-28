6e7ee0ad9effdd4418de01fd87f720431b61cb2e
Mickael <mickael@popina.com>
2022-03-28 08:58:58 +0200

    Ajout d'un composant ColorPicker

--- components/ColorPicker.tsx
+++ components/ColorPicker.tsx
@@ -1,56 +1,21 @@
 import { Button } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ColorPicker as ColorPickup, useColor } from "react-color-palette";
 import "react-color-palette/lib/css/styles.css";
 import Modal from 'react-modal';
+import Edit from '../pages/edit';
 
 export default function ColorPicker(props: any) {
 
-    const [showModal, setShowModal] = useState<boolean>(false)
-
-    const customStyles = {
-
-        content: {
-          top: '50%',
-          left: '50%',
-          right: 'auto',
-          bottom: 'auto',
-          marginRight: '-50%',
-          transform: 'translate(-50%, -50%)',
-        },
-    };
-
     const [color, setColor] = useColor("hex", "#121212");
-    const [modalIsOpen, setIsOpen] = useState(false);
 
-    const openModal = () => {
-        setIsOpen(true);
-    }
-
-    
-    const closeModal = () => {
-        setIsOpen(false);
-    }
-
-    const handleClickOpen = () => {
-
-        setShowModal(props.modal)
-
-    }
+    useEffect(() => {
+        <Edit color={color["hex"]} />
+    }, [color])
 
     return (
         <div>
-        <Button variant='outlined' onClick={handleClickOpen}> Ajouter une couleur</Button>
-        { showModal === true &&
-        <Modal
-        isOpen={modalIsOpen}
-        style={customStyles}
-        >
-
-        <ColorPickup width={456} height={228} color={color} onChange={setColor} hideHSV dark />
-        
-        </Modal>
-        }
+        <ColorPickup width={256} height={228} color={color} onChange={setColor} hideHSV dark />
         </div>
       
     )
--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,34 +1,50 @@
-import { Button } from '@mantine/core';
-import React from 'react';
+import React, { useEffect } from 'react';
 import ColorPicker from '../components/ColorPicker';
 import styles from '../styles/Home.module.css'
-import {Scene, WebGL1Renderer, PerspectiveCamera, CylinderGeometry, MeshBasicMaterial, Mesh} from 'three'
+import {Scene, WebGL1Renderer, PerspectiveCamera, CylinderGeometry, MeshBasicMaterial, Mesh, ColorRepresentation} from 'three'
+import { Color } from 'react-color-palette';
 
 const scene = new Scene()
 const renderer = new WebGL1Renderer()
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
-const Edit = () => {
+interface EditInterface {
+    color: Color['hex']
+}
 
-    const geometry = new CylinderGeometry(5, 5, 10, 32)
-    const material = new MeshBasicMaterial({color: 0x00a6ff, wireframe: true})
-    const cylinder = new Mesh(geometry, material)
+const Edit = (props: EditInterface) => {
 
-    scene.add(cylinder)
-    camera.position.z = 20;
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(renderer.domElement)
+    const {color} = props;
+
+//    const updateColor = () => {
+//    const newColor = color.slice(1) as unknown as ColorRepresentation
+//    console.log(newColor)
+//        new MeshBasicMaterial({color: newColor});
+//        return newColor;   
+//    }
+//
+//    updateColor()
+
+    useEffect(() => {
+        const geometry = new CylinderGeometry(5, 5, 10, 32)
+        const material = new MeshBasicMaterial({color: 0x00FFF, wireframe: true})
+        const cylinder = new Mesh(geometry, material)
+        scene.add(cylinder)
+        camera.position.z = 20;
+        renderer.setSize(window.innerWidth, window.innerHeight)
+        document.body.appendChild(renderer.domElement)
+
+        const animate = () => {
+            cylinder.rotation.x += 0.01
+            cylinder.rotation.y += 0.01
     
+            renderer.render(scene, camera);
+            requestAnimationFrame(animate)
+        }
 
-    const animate = () => {
-        cylinder.rotation.x += 0.01
-        cylinder.rotation.y += 0.01
+        animate()
 
-        renderer.render(scene, camera);
-        requestAnimationFrame(animate)
-    }
-
-    animate()
+    }, [])
 
     return (
         <div className={styles.card}>
