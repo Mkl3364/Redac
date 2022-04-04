43170f6dd70a61b1884eab11d580e90e44a865d0
Mickael <mickael@popina.com>
2022-04-04 10:21:56 +0200

    Ajout feature ajout de composant

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -1,14 +1,20 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Box from '../components/Box'
 import Header from '../components/Header';
 import ColorPicker from '../components/ColorPicker';
 import ColorPickerContext from '../context/ColorPickerContext';
 import ColorPickerProvider from '../context/ColorPickerProvider';
+import { Button } from '@mantine/core';
 
 const edit = () => {
 
     const color = useContext(ColorPickerContext)
+    const [boxState, setBoxState] = useState<boolean>(false)
+
+    const handleClickAddBox = () => {
+        setBoxState(!boxState);
+    }
 
     return (
         <>
@@ -16,12 +22,14 @@ const edit = () => {
             <ColorPickerProvider>
             <div>
             <ColorPicker />
+            <Button variant="light" color="blue" onClick={handleClickAddBox}> Ajoute un composant </Button>
             </div>
             <Canvas style={{ height: '1000px' }}>
                 <ambientLight />
                 <pointLight position={[10, 10, 10]} />
                 <Box position={[-1.2, 0, 0]} />
                 <Box position={[1.2, 0, 0]} />
+                {boxState ? <Box position={[3.9, 0, 0]} /> : '' }
             </Canvas>
             </ColorPickerProvider>
         </>
