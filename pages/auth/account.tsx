145723da19b7c2aed019aa49c1b1ee8cc862e72f
Mickael <mickael@popina.com>
2022-04-13 07:52:06 +0200

    Modification page profil et ajout page account

---
+++ pages/auth/account.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//TODO
+// Récuperer les infos utilisateurs de firebase et les modifier depuis l'app
+
+const account = () => {
+    return (
+        <div>
+            <h1>Account</h1>
+            
+        </div>
+    );
+};
+
+export default account;
